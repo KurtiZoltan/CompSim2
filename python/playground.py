0a1446a18811cf3bf5eadd42eb972a34837eddb3
Kürti Zoltán <kuzo1998@gmail.com>
2021-11-12 23:29:00 +0100

    N dimensional magical kernel factory

--- python/playground.py
+++ python/playground.py
@@ -55,52 +55,94 @@ def derivatives(variableCount, order):
                 ret.append(a)
     return ret
 
+def kernelPoints(radius, dim):
+    if dim == 1:
+        return [[i] for i in range(radius + 1)]
+    else:
+        ret = []
+        ending = kernelPoints(radius, dim - 1)
+        for i in range(radius + 1):
+            for end in ending:
+                if i <= end[0]:
+                    new = end.copy()
+                    new.insert(0, i)
+                    ret.append(new)
+    return ret
+
 def extendBySymmetry(positions, symmetryGenerator):
     for p in positions:
         if symmetryGenerator(p) not in positions:
             positions.append(symmetryGenerator(p))
 
-def kernel2D(radius, order):
+def mirror(p, coordinate):
+    p[coordinate] *= -1
+    return p
+
+def swap(p, i, j):
+    temp = p[i]
+    p[i] = p[j]
+    p[j] = temp
+    return p
+
+def kernelNDMatrix(radius, order, dim):
     columns = []
-    for i in range(radius+1):
-        for j in range(i, radius+1):
-            positionList = [[i, j]]
-            extendBySymmetry(positionList, lambda p: [-p[0], p[1]])
-            extendBySymmetry(positionList, lambda p: [p[0], -p[1]])
-            extendBySymmetry(positionList, lambda p: [p[1], p[0]])
-            
-            taylors = [taylorPolinom(p, order) for p in positionList]
-            taylor = [0] * len(taylors[0])
-            for t in taylors:
-                taylor = [x + y[0] for x, y in zip(taylor, t)]
-            columns.append(taylor)
+    for position in kernelPoints(radius, dim):
+        positionList = [position]
+        for i in range(dim):
+            extendBySymmetry(positionList, lambda p: mirror(p.copy(), i))
+        for i in range(dim):
+            for j in range(i, dim):
+                extendBySymmetry(positionList, lambda p: swap(p.copy(), i, j))
+                
+        taylors = [taylorPolinom(p, order) for p in positionList]
+        taylorSum = [0] * len(taylors[0])
+        for t in taylors:
+            taylorSum = [x + y[0] for x, y in zip(taylorSum, t)]
+        columns.append(taylorSum)
     return np.array(columns).transpose()
 
-def kernel3D(radius, order):
-    columns = []
-    for i in range(radius+1):
-        for j in range(i, radius+1):
-            for k in range(j, radius+1):
-                positionList = [[i, j, k]]
-                extendBySymmetry(positionList, lambda p: [-p[0], p[1], p[2]])
-                extendBySymmetry(positionList, lambda p: [p[0], -p[1], p[2]])
-                extendBySymmetry(positionList, lambda p: [p[0], p[1], -p[2]])
-                extendBySymmetry(positionList, lambda p: [p[0], p[2], p[1]])
-                extendBySymmetry(positionList, lambda p: [p[1], p[0], p[2]])
-                extendBySymmetry(positionList, lambda p: [p[1], p[2], p[0]])
-                extendBySymmetry(positionList, lambda p: [p[2], p[0], p[1]])
-                extendBySymmetry(positionList, lambda p: [p[2], p[1], p[0]])
-                
-                taylors = [taylorPolinom(p, order) for p in positionList]
-                taylor = [0] * len(taylors[0])
-                for t in taylors:
-                    taylor = [x + y[0] for x, y in zip(taylor, t)]
-                columns.append(taylor)
-    return np.array(columns).transpose()
+def kernel2DMatrix(radius, order):
+    return kernelNDMatrix(radius, order, 2)
+
+def kernel3DMatrix(radius, order):
+    return kernelNDMatrix(radius, order, 3)
+
+def kernelFromCoeffs (a, radius, dim):
+    ret = np.zeros(shape=[2*radius+1]*dim)
+    points = kernelPoints(radius, dim)
+    for idx in np.ndindex(ret.shape):
+        key = np.array(idx)
+        key = key - radius
+        key = np.abs(key)
+        key = sorted(key)
+        key = list(key)
+        ret[idx] = a[points.index(key)]
+    return ret
+
+def laplace(order, dim):
+    basis = derivatives(dim, order)
+    ret = np.zeros((len(basis)))
+    for i, b in enumerate(basis):
+        for j in range(dim):
+            secondDiff = [0] * dim
+            secondDiff[j] = 2
+            if b == secondDiff:
+                ret[i] = 1
+    return ret
+
+def laplaceKernelsND(radius, dim):
+    order = 2 * radius
+    A = kernelNDMatrix(radius, order, dim)
+    b = laplace(order, dim)
+    x, residuals, rank, s = np.linalg.lstsq(A, b, rcond=None)
+    nullSpace = null_space(A)
+    return x, nullSpace
+
+
 
 def smallestError2D(radius):
     order = radius * 2
-    A = kernel2D(radius, order)
+    A = kernel2DMatrix(radius, order)
     basis = derivatives(2, order)
     b = np.zeros((len(basis)))
     for i, diff in enumerate(basis):
@@ -117,7 +159,7 @@ def smallestError2D(radius):
     #print("Result: ", list(filter(lambda x: abs(x[0]) > 1e-6, [[x, y] for x, y in zip(result, basis)])))
 
     #a0 + nullspace @ x are the solutions
-    A = kernel2D(radius, order + 4)
+    A = kernel2DMatrix(radius, order + 4)
     basis = derivatives(2, order + 4)
     b = np.zeros((len(basis)))
     for i, diff in enumerate(basis):
@@ -132,7 +174,7 @@ def smallestError2D(radius):
     print(f"Final result for estimate:\n", list(filter(lambda x: abs(x[0]) > 1e-6, [[x, y] for x, y in zip(result, basis)])))
     return a0 + nullspace @ x
 
-def drawKernel2D(a, radius):
+def drawKesrnel2D(a, radius):
     coeffs = np.zeros((2*radius+1, 2*radius+1))
     for i in range(-radius, radius + 1):
         for j in range(-radius, radius + 1):
@@ -147,6 +189,8 @@ def drawKernel2D(a, radius):
     plt.colorbar()
     plt.show()
 
-radius = 3
-a = smallestError2D(radius)
-drawKernel2D(a, radius)
+radius = 2
+#a = smallestError2D(radius)
+#drawKernel2D(a, radius)
+x, nullspace = laplaceKernelsND(radius, 10)
+print(nullspace.shape)
