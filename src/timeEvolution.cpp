ab1091c313f828c8c3a75a1693878398c3b7a423
Kürti Zoltán <kuzo1998@gmail.com>
2021-11-18 21:59:08 +0100

    Calculate laplace

--- src/timeEvolution.cpp
+++ src/timeEvolution.cpp
@@ -1,6 +1,7 @@
 #include "Utils.hpp"
 #include <iostream>
 #include <cmath>
+#include <utility>
 
 //-------------------------------------------------------------------------------------------------
 //helper functions
@@ -21,13 +22,13 @@ void freeGrid(T* grid)
     delete[] grid;
 }
 
-void fillRegion(u8* region, bool (*f)(u32 i, u32 j, u32 n, u32 m), u32 n, u32 m)
+void initRegion(u8* region, bool (*f)(u32 i, u32 j), u32 n, u32 m)
 {
     for (u32 i = 0; i < n; i++)
     {
         for (u32 j = 0; j < m; j++)
         {
-            region[j * m + i] = f(i, j, n, m) ? 1 : 0;
+            region[j * m + i] = f(i, j) ? 1 : 0;
         }
     }
     for (u32 d = 2; 2 * d < n && 2 * d < m && d < 256; d++)
@@ -54,11 +55,16 @@ void fillRegion(u8* region, bool (*f)(u32 i, u32 j, u32 n, u32 m), u32 n, u32 m)
 
 struct BoundaryElement
 {
+    u8 type; // 0 - Dirichlet, 1 - Neumann
     u32 i, j;
-    f64 nx, ny;
+    union 
+    {
+        f64 normal[2];
+        f64 value;
+    };
 };
 
-BoundaryElement* allocateBoundaryNormals(u8* region, u32 n, u32 m, void (*normal)(u32 i, u32 j, u32 n, u32 m, f64 dx, f64 dy, f64& nx, f64& ny), f64 dx, f64 dy)
+std::pair<BoundaryElement*, u32> allocateBoundary(u8* region, u32 n, u32 m, BoundaryElement (*boundary)(u32 i, u32 j), f64 dx, f64 dy)
 {
     u32 boundaryCount = 0;
     for (u32 i = 0; i < n; i++)
@@ -80,55 +86,137 @@ BoundaryElement* allocateBoundaryNormals(u8* region, u32 n, u32 m, void (*normal
         {
             if (region[j * n + i] == 1)
             {
-                boundaryElements[boundaryCount].i = i;
-                boundaryElements[boundaryCount].j = j;
-                normal(i, j, n, m, dx, dy, boundaryElements[boundaryCount].nx, boundaryElements[boundaryCount].ny);
+                boundaryElements[boundaryCount] = boundary(i, j);
                 boundaryCount++;
             }
         }
     }
-    return boundaryElements;
+    return std::make_pair(boundaryElements, boundaryCount);
 }
 
-void freeBoundaryNormals(BoundaryElement* normals)
+void freeBoundary(BoundaryElement* normals)
 {
     delete[] normals;
 }
 
+void laplace35(f64* gridIn, u8* region, f64* gridOut, u32 n, u32 m, f64 h)
+{
+    for (u32 i = 0; i < n; i++)
+    {
+        for (u32 j = 0; j < m; j++)
+        {
+            f64 temp = 0;
+            if (region[j * n + i] == 2)
+            {
+                temp = (
+                    (gridIn[(j - 1) * n + i - 1] + gridIn[(j - 1) * n + i + 1] + gridIn[(j + 1) * n + i - 1] + gridIn[(j + 1) * n + i + 1]) + 
+                    (gridIn[j * n + i - 1] + gridIn[j * n + i + 1] + gridIn[(j - 1) * n + i] + gridIn[(j + 1) * n + i]) * 4 +
+                    -(gridIn[j * n + i]) * 20
+                    ) / 6;
+            }
+            else if (region[j * n + i] > 2)
+            {
+                temp = (
+                    -(gridIn[j * n + i - 2] + gridIn[j * n + i + 2] + gridIn[(j - 2) * n + i] + gridIn[(j + 2) * n + i]) * 1 +
+                    -(gridIn[(j + 1) * n + i - 2] + gridIn[(j + 1) * n + i + 2] + gridIn[(j - 2) * n + i + 1] + gridIn[(j + 2) * n + i + 1] +
+                      gridIn[(j - 1) * n + i - 2] + gridIn[(j - 1) * n + i + 2] + gridIn[(j - 2) * n + i - 1] + gridIn[(j + 2) * n + i - 1]) * 2 +
+                    (gridIn[(j - 1) * n + i - 1] + gridIn[(j - 1) * n + i + 1] + gridIn[(j + 1) * n + i - 1] + gridIn[(j + 1) * n + i + 1]) * 16 + 
+                    (gridIn[j * n + i - 1] + gridIn[j * n + i + 1] + gridIn[(j - 1) * n + i] + gridIn[(j + 1) * n + i]) * 52 +
+                    -(gridIn[j * n + i]) * 252
+                    ) / 60;
+            }
+            gridOut[j * n + i] = temp / h / h;
+        }
+    }
+}
+
+void laplace3(f64* gridIn, u8* region, f64* gridOut, u32 n, u32 m, f64 h)
+{
+    for (u32 i = 0; i < n; i++)
+    {
+        for (u32 j = 0; j < m; j++)
+        {
+            f64 temp = 0;
+            if (region[j * n + i] > 1)
+            {
+                temp = (
+                    (gridIn[(j - 1) * n + i - 1] + gridIn[(j - 1) * n + i + 1] + gridIn[(j + 1) * n + i - 1] + gridIn[(j + 1) * n + i + 1]) + 
+                    (gridIn[j * n + i - 1] + gridIn[j * n + i + 1] + gridIn[(j - 1) * n + i] + gridIn[(j + 1) * n + i]) * 4 +
+                    -(gridIn[j * n + i]) * 20
+                    ) / 6;
+            }
+            gridOut[j * n + i] = temp / h / h;
+        }
+    }
+}
+
+void laplace3ani(f64* gridIn, u8* region, f64* gridOut, u32 n, u32 m, f64 h)
+{
+    for (u32 i = 0; i < n; i++)
+    {
+        for (u32 j = 0; j < m; j++)
+        {
+            f64 temp = 0;
+            if (region[j * n + i] > 1)
+            {
+                temp = 
+                    (gridIn[j * n + i - 1] + gridIn[j * n + i + 1] + gridIn[(j - 1) * n + i] + gridIn[(j + 1) * n + i]) +
+                    -(gridIn[j * n + i]) * 4;
+            }
+            gridOut[j * n + i] = temp / h / h;
+        }
+    }
+}
+
+void applyBoundary(f64* grid, BoundaryElement* boundary, u32 boundaryCount)
+{
+    
+}
+
 //-------------------------------------------------------------------------------------------------
 
-bool circle(u32 i, u32 j, u32 n, u32 m)
+u32 N = 1024;
+
+bool circle(u32 i, u32 j)
 {
-    f64 x = 2 * (static_cast<f64>(i) / (n - 1) - 0.5);
-    f64 y = 2 * (static_cast<f64>(j) / (m - 1) - 0.5);
+    f64 x = 2 * (static_cast<f64>(i) / (N - 1) - 0.5);
+    f64 y = 2 * (static_cast<f64>(j) / (N - 1) - 0.5);
     f64 R = 0.9;
     return x * x + y * y < R * R;
 }
 
-void circleNormal(u32 i, u32 j, u32 n, u32 m, f64 dx, f64 dy, f64& nx, f64& ny)
+BoundaryElement circleBoundary(u32 i, u32 j)
 {
-    f64 x = 2 * (static_cast<f64>(i) / (n - 1) - 0.5);
-    f64 y = 2 * (static_cast<f64>(j) / (m - 1) - 0.5);
-    nx = -x / dx;
-    ny = -y / dy;
+    f64 nx = 2 * (static_cast<f64>(i) / (N - 1) - 0.5);
+    f64 ny = 2 * (static_cast<f64>(j) / (N - 1) - 0.5);
     f64 norm = std::sqrt(nx * nx + ny * ny);
     nx /= norm;
     ny /= norm;
+    BoundaryElement ret;
+    ret.type = 1;
+    ret.i = i;
+    ret.j = j;
+    ret.normal[0] = nx;
+    ret.normal[1] = ny;
+    return ret;
 }
 
 int main()
 {
-    u32 N = 1024;
-    f64 h = 1.0 / (N - 1);
+    f64 dx = 1.0 / (N - 1);
     f64* T = allocateGrid<f64>(N, N);
+    f64* laplace = allocateGrid<f64>(N, N);
     u8* region = allocateGrid<u8>(N, N);
-    fillRegion(region, circle, N, N);
-    BoundaryElement* normals = allocateBoundaryNormals(region, N, N, circleNormal, h, h);
+    initRegion(region, circle, N, N);
+    auto ret = allocateBoundary(region, N, N, circleBoundary, dx, dx);
+    BoundaryElement* boundary = ret.first;
+    u32 boundaryCount = ret.second;
     
     
     
-    freeBoundaryNormals(normals);
+    freeBoundary(boundary);
     freeGrid(region);
+    freeGrid(laplace);
     freeGrid(T);
     return 0;
 }
